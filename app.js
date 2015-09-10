9b3b10eb38983469290642bee2f489c272bac1df
donte <digitalartcrew@gmail.com>
2015-09-10 10:27:44 -0700

    Added Routes

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.get('/', function(req,res){
 res.render('index',{name:"Puppy"});
 });
 
+
+// To get a form to save a new puppy
+app.get('/puppies/new', function(){});
+
+//To save a new puppy
+app.get('/puppies', function(){});
+
+//To Find a puppy by ID
+app.get('/puppies', function(){});
+
+app.get('/about', function(){});
+
+app.get('/contact', function(){});
+
 app.listen(3000, function(){
 	console.log("Server running on port 3000");
 });
